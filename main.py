9e75ac7e37f0722532959dc858ebef7fffddfaf1
kameker <vsrnpra2@gmail.com>
2022-05-09 11:08:22 +0300

    Пейзажи Марса

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/carousel')
+def greeting():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
