7c1626d8ede14c483d1e2f7eec459406e81a10fb
kameker <vsrnpra2@gmail.com>
2022-05-09 11:53:44 +0300

    загрузка фото

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from base64 import b64encode
+
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
@@ -12,6 +14,20 @@ def index():
 @app.route('/carousel')
 def greeting():
     return render_template('index.html')
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'GET':
+        return render_template('load.html', title='Отбор астронавтов')
+    elif request.method == 'POST':
+        image = request.files['file']
+        bytes = image.read()
+        image = b64encode(bytes).decode('utf-8')
+        with open("static/img/picture.txt", "w") as f:
+            f.write(image)
+            f.close()
+        return render_template('load.html', title='Отбор астронавтов', image=image)
 
 
 if __name__ == '__main__':
